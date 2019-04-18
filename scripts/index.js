0a11359db7f45eb144d1d84024630c3b513fc4a6
Steven Huang <steven.charles.huang@gmail.com>
2019-04-18 13:03:11 -0400

    player and opponent boards are generating correctly with tiles

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,69 @@
 $(document).ready(() => {
-  console.log('jQ Ready');
+  // jQ Selectors
+  const 
+    $gameArea = $('.game-area'),
+    $gameBoard = $('.game-board')
+    $playerBoard = $('#player-board');
+    $opponentBoard = $('#opponent-board');
+    let $playerTile;
+    let $opponentTile;
+    let $tile;
+
+  /**
+   * @function createBoards
+   * @description Creates the Player and Opponent boards and fills with tiles
+   */
+  const createBoards = () => {
+    for (let i = 0; i < 100; i += 1) {
+      let playerTile = 
+        `<div 
+          class="tile player-tile"
+          data-type="player" 
+          data-tile="${i}" 
+          data-id="${i}" 
+          id="player-${i}">
+        </div>`;
+
+      let opponentTile = 
+        `<div 
+          class="tile opponent-tile"
+          data-type="opponent" 
+          data-tile="${i}" 
+          data-id="${i}" 
+          id="opponent-${i}">
+        </div>`;
+
+      $($opponentBoard).append(opponentTile);
+      $($playerBoard).append(playerTile);
+    }
+    
+    // Update jQ Selector Vars
+    $playerTile = $('.player-tile');
+    $opponentTile = $('.opponent-tile');
+
+    // Set tile click handlers
+    $($playerTile).on('click', handlePlayerTileClick);
+    $($opponentTile).on('click', handleOpponentTileClick);
+  };
+
+  /**
+   * @function handleOpponentTileClick
+   * @description Handles the click event for opponent tiles
+   * @param {object} e 
+   */
+  const handleOpponentTileClick = (e) => {
+    console.log('Opponent tile clicked', e);
+  };
+
+  /**
+   * @function handlePlayerTileClick
+   * @description Handles the click event for player tiles
+   * @param {object} e 
+   */
+  const handlePlayerTileClick = (e) => {
+    console.log('Player tile clicked', e);
+  };
+
+
+  createBoards();
 });
